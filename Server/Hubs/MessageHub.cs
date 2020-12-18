dcc04742f6c9d561f3d70c0ea25375d77ab3f1a9
luan goedert <luan_goedert2013@hotmail.com>
2020-12-17 23:23:43 -0300

    ponto

---
+++ Server/Hubs/MessageHub.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using SignalRClr.Shared;
+using System.Threading.Tasks;
+namespace SignalRClr.Server.Hubs
+{
+    public class MessageHub : Hub
+    {
+        public async Task SendMessage(Message message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", message);
+        }
+    }
+}
